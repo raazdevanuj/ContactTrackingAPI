dc4e6c2bf0cde10a42db77f37c7e86dac4f51b87
raazdevanuj <raazdevanuj@gmail.com>
2020-12-12 14:09:44 +0530

    after connection establish

--- main.go
+++ main.go
@@ -1,39 +1,98 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"path"
+	"strconv"
+	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //users .....
 type Users struct {
-	ID          int    `json:"id"`
-	NAME        string `json:"name"`
-	DateOfBirth string `json:"dateofbirth"`
-	PhoneNumber int32  `json:"phonenumber"`
-	EMAIL       string `json:"email"`
-	Timestamp   string `json:"timestamp"`
+	ID          int       `json:"id"`
+	NAME        string    `json:"name"`
+	DateOfBirth string    `json:"dateofbirth"`
+	PhoneNumber int32     `json:"phonenumber"`
+	EMAIL       string    `json:"email"`
+	Timestamp   time.Time `json:"timestamp"`
 }
 
 //contacts...
 type Contacts struct {
-	USERIDONE int    `json:"useridone"`
-	USERIDTWO int    `json:"useridtwo"`
-	Timestamp string `json:"timestamp"`
+	USERIDONE int       `json:"useridone"`
+	USERIDTWO int       `json:"useridtwo"`
+	Timestamp time.Time `json:"timestamp"`
 }
-type userss []Users
 
-// fmt.Println("Endpoint Hit: all articles end")
-// json.NewEncoder(w).Encode(articles)
+var clientInstance *mongo.Client
+var clientInstanceError error
+var mongoOnce sync.Once
+
+const (
+	CONNECTIONSTRING = "mongodb://localhost:27017"
+	DB               = "db_issue_manager"
+	ISSUES           = "col_issues"
+)
+
+func GetMongoClient() (*mongo.Client, error) {
+	mongoOnce.Do(func() {
+
+		clientOptions := options.Client().ApplyURI(CONNECTIONSTRING)
+
+		client, err := mongo.Connect(context.TODO(), clientOptions)
+		if err != nil {
+			clientInstanceError = err
+		}
+
+		err = client.Ping(context.TODO(), nil)
+		if err != nil {
+			clientInstanceError = err
+		}
+		clientInstance = client
+	})
+	return clientInstance, clientInstanceError
+}
+
+/* fmt.Println("Endpoint Hit: all articles end")
+json.NewEncoder(w).Encode(articles)*/
+
+func getbyid(code string) (Users, error) {
+	result := Users{}
+	i, err := strconv.Atoi(code)
+	filter := bson.D{primitive.E{Key: "ID", Value: i}}
+	client, err := GetMongoClient()
+	if err != nil {
+		return result, err
+	}
+	collection := client.Database(DB).Collection(ISSUES)
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
+}
 
 func viewuserutil(w http.ResponseWriter, r *http.Request) {
 	//fmt.Printf("Req: %s %s", r.URL, r.URL.Path)
 	var a string = r.URL.Path
-
-	fmt.Println(path.Base(a))
+	var user, err = getbyid(path.Base(a))
+	if err != nil {
+		fmt.Fprintf(w, "Error")
+		return
+	}
+	json.NewEncoder(w).Encode(user)
 }
 func createuserutil(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Test Post created")
